a4f46a51ca8099d53ccd98dfe490bf330e1e82db
Joshua Lugo <atwfaj@gmail.com>
2016-02-09 19:26:41 +0000

    Update address/format_address_spec.rb, add math_methods_spec.rb and my_solution.rb

---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,22 @@
+# Math Methods
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def add(num_1, num_2)
+ num_1 + num_2 = num_3
+end
+
+def subtract(num_1, num_2)
+ num_1 - num_2 = num_3
+end
+
+def multiply(num_1, num_2)
+ num_1 * num_2 = num_3
+end
+
+def divide(num_1, num_2)
+ num_1.to_f / num_2.to_f = num_3
+end
