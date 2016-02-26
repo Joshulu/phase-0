cf3b600061706ddb5e4f831b03cd6d1b08927c01
Joshua Lugo <atwfaj@gmail.com>
2016-02-26 21:29:08 +0000

    Update my_solution.rb with release questions

--- week-6/attr/my_solution.rb
+++ week-6/attr/my_solution.rb
@@ -1,6 +1,6 @@
 #Attr Methods
 
-# I worked on this challenge [by myself, with:]
+# I worked on this challenge [by myself]
 
 # I spent [#] hours on this challenge.
 
@@ -22,4 +22,36 @@ end
 
 
 # Reflection
+=begin
+Release 1:
+-What are these methods doing?
+
+
+-How are they modifying or returning the value of instance varialbes?
+
+
+Release 2:
+-What changed between the last release and this release?
+
+
+-What was replaced?
+
+
+-Is this code simpler than the last?
+
+
+Release: 3
+-What changed between the last release and this release?
+
+
+-What was replaced?
+
+
+-Is this code simpler than the last?
+
+
+
+
+
+
 
