0b855d32fe6610662427b656e66d841cda0f425d
Joshua Lugo <atwfaj@gmail.com>
2016-03-02 23:20:04 +0000

    Complete Release 1 intro and Ch.1

--- week-7/eloquent.js
+++ week-7/eloquent.js
@@ -1,6 +1,51 @@
 // Eloquent JavaScript
+Introduction
+-Did you learn anything new about JavaScript or programming in general?
 
-// Run this file in your terminal using `node my_solution.js`. Make sure it works before moving on!
+  No not really.  Nothing I didn't expect came out of the wood works in the Intro.
+
+-Are there any concepts you feel uncomfortable with?
+
+  So far, no.  Nothing is out of the blue, like I said before, it should be different, but still object oriented.
+
+Ch. 1
+-Identify two similarities and two differences between JavaScript and Ruby syntax with regard to numbers, arithmetic, strings, booleans, and various operators.
+
+  Similar: 1. They both allow standard operations to be used on numbers (+,-,/,*,%).
+  Similar: 2. Comparison (<,>,etc) and logical operators (||, &&) are evaluated the same way.
+  Difference: 1. JS takes more user error, but tries to work with it, using (apparantly complex) ways to change datatypes to other datatypes.
+  Difference: 2. JS numbers are all float numbers.  Integer division is not the same as it is in Ruby, unless you write extra code.
+
+Ch. 2
+-What is an expression?
+
+
+-What is the purpose of semicolons in JavaScript? Are they always required?
+
+
+-What causes a variable to return undefined?
+
+
+-Write your own variable and do something to it in the eloquent.js file.
+
+
+-What does console.log do and when would you use it? What Ruby method(s) is this similar to?
+
+
+-Write a short program that asks for a user to input their favorite food. After they hit return, have the program respond with "Hey! That's my favorite too!" (You will probably need to run this in the Chrome console (Links to an external site.) rather than node since node does not support prompt or alert). Paste your program into the eloquent.js file.
+
+
+-Describe while and for loops
+
+
+-What other similarities or differences between Ruby and JavaScript did you notice in this section?
+
+
+-Complete at least one of the exercises (Looping a Triangle, FizzBuzz, of Chess Board) in the eloquent.js file.
+
+
+
+// Run this file in your terminal using `node my_solution.js`(eloquent.js). Make sure it works before moving on!
 
 // Program Structure
 // Write your own variable and do something to it.
