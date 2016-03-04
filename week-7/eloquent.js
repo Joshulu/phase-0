3de7bf78aa5c65a4da48443aa2a394fa894db36b
Joshua Lugo <atwfaj@gmail.com>
2016-03-04 00:38:16 +0000

    Update eloquent.js

--- week-7/eloquent.js
+++ week-7/eloquent.js
@@ -1,67 +1,41 @@
 // Eloquent JavaScript
-Introduction
--Did you learn anything new about JavaScript or programming in general?
-
-  No not really.  Nothing I didn't expect came out of the wood works in the Intro.
-
--Are there any concepts you feel uncomfortable with?
-
-  So far, no.  Nothing is out of the blue, like I said before, it should be different, but still object oriented.
-
-Ch. 1
--Identify two similarities and two differences between JavaScript and Ruby syntax with regard to numbers, arithmetic, strings, booleans, and various operators.
-
-  Similar: 1. They both allow standard operations to be used on numbers (+,-,/,*,%).
-  Similar: 2. Comparison (<,>,etc) and logical operators (||, &&) are evaluated the same way.
-  Difference: 1. JS takes more user error, but tries to work with it, using (apparantly complex) ways to change datatypes to other datatypes.
-  Difference: 2. JS numbers are all float numbers.  Integer division is not the same as it is in Ruby, unless you write extra code.
-
-Ch. 2
--What is an expression?
-
-
--What is the purpose of semicolons in JavaScript? Are they always required?
-
-
--What causes a variable to return undefined?
-
-
--Write your own variable and do something to it in the eloquent.js file.
-
-
--What does console.log do and when would you use it? What Ruby method(s) is this similar to?
-
-
--Write a short program that asks for a user to input their favorite food. After they hit return, have the program respond with "Hey! That's my favorite too!" (You will probably need to run this in the Chrome console (Links to an external site.) rather than node since node does not support prompt or alert). Paste your program into the eloquent.js file.
-
-
--Describe while and for loops
-
-
--What other similarities or differences between Ruby and JavaScript did you notice in this section?
-
-
--Complete at least one of the exercises (Looping a Triangle, FizzBuzz, of Chess Board) in the eloquent.js file.
-
 
 
 // Run this file in your terminal using `node my_solution.js`(eloquent.js). Make sure it works before moving on!
 
 // Program Structure
 // Write your own variable and do something to it.
+var energy = 100
+var chore_energy = 20
+energy = energy - chore_energy
 
-
-
+// Prompt program
+var food = String(prompt("What's your favorite food?"));
+alert("That's my favorite food too!")
 
 // Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
-
+for(var sign = "#"; sign.length < 8; sign = sign + "#")
+  {console.log(sign);}
 
 
 // Functions
 
 // Complete the `minimum` exercise.
 
-
+var minimum = function(x, y){
+  if (x < y)
+    return x;
+  else
+    return y;
+}
 
 // Data Structures: Objects and Arrays
 // Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+
+var me = {
+  name: "Joshua",
+  age: 22,
+  fav_foods: ["pho", "tacos", "shrimp"],
+  quirk: "I'm always happy to be somewhere, but never happy to leave where I am.",
+  };
+
